d41e86f1446f74b72a3db5c5da6f4439cc92185f
Jessie S. Caminos <61927149+jsonimac@users.noreply.github.com>
2023-09-15 14:42:41 +0800

    Update seo.js

--- src/data/seo.js
+++ src/data/seo.js
@@ -2,36 +2,36 @@ const SEO = [
 	{
 		page: "home",
 		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
+			"Having successfully completed for almost two-year career as a Quality Assurance Engineer, I am enthusiastic about embarking on a fresh path as a Software Developer. With a genuine passion for coding and an unwavering commitment to ongoing learning, I am fully prepared to embrace the challenges and opportunities that accompany this thrilling career transition.",
+		keywords: ["Jessie", "Jessie C.", "Jessie Caminos"],
 	},
 
 	{
 		page: "about",
 		description:
-			"I am a backend developer with expertise in Node.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
+			"I hold a Bachelor of Science degree in Information Technology, a foundational credential that has provided me with a strong theoretical background in the field. With nearly two years of professional experience as a Quality Assurance Engineer, I have honed my skills in ensuring the reliability and functionality of software products. Additionally, I have taken the initiative to further develop my expertise by delving into Fullstack Development, expanding my skill set to encompass both front-end and back-end technologies. This journey has allowed me to bring a holistic perspective to web development projects, combining my QA expertise with hands-on coding abilities to deliver high-quality, well-rounded solutions.",
+		keywords: ["Jessie", "Jessie C.", "Jessie Caminos"],
 	},
 
-	{
-		page: "articles",
-		description:
-			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
-	},
+	// {
+	// 	page: "articles",
+	// 	description:
+	// 		"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	// 	keywords: ["Jessie", "Jessie C.", "Jessie Caminos"],
+	// },
 
 	{
 		page: "projects",
 		description:
 			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
+		keywords: ["Jessie", "Jessie C.", "Jessie Caminos"],
 	},
 
 	{
 		page: "contact",
 		description:
 			"If you're interested in collaborating on a project, feel free to reach out to me. I'm always open to new ideas and opportunities.",
-		keywords: ["Tharindu", "Tharindu N", "Tharindu Nayanajith"],
+		keywords: ["Jessie", "Jessie C.", "Jessie Caminos"],
 	},
 ];
 
