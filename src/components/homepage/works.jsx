ba9d9bb3c432e2fd4a1b8554fa1ba977c54feff2
Jessie S. Caminos <61927149+jsonimac@users.noreply.github.com>
2023-09-16 23:45:29 +0800

    update display

--- src/components/homepage/works.jsx
+++ src/components/homepage/works.jsx
@@ -15,28 +15,15 @@ const Works = () => {
 					<div className="works-body">
 						<div className="work">
 							<img
-								src="./facebook.png"
-								alt="facebook"
+								src="https://scontent.fcgy2-2.fna.fbcdn.net/v/t39.30808-1/278498238_294100662908872_8539355154255964375_n.jpg?stp=dst-jpg_p320x320&_nc_cat=101&ccb=1-7&_nc_sid=754033&_nc_ohc=KkzorBzeZX4AX-Ms6HO&_nc_ht=scontent.fcgy2-2.fna&oh=00_AfARwY0JyrGrOry1FRkZR7ZCsbnzLlh4s4jWrYxRzeAuhw&oe=650B6D11"
+								alt="human incubator"
 								className="work-image"
 							/>
-							<div className="work-title">Facebook</div>
+							<div className="work-title">Human Incubator Inc.</div>
 							<div className="work-subtitle">
-								Software Engineer
+								Quality Assurance Engineer
 							</div>
-							<div className="work-duration">2019 - Present</div>
-						</div>
-
-						<div className="work">
-							<img
-								src="./twitter.png"
-								alt="twitter"
-								className="work-image"
-							/>
-							<div className="work-title">Twitter</div>
-							<div className="work-subtitle">
-								Software Engineer
-							</div>
-							<div className="work-duration">2019 - Present</div>
+							<div className="work-duration">2021 - Present</div>
 						</div>
 					</div>
 				}
