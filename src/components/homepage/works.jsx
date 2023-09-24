ea1dde0fc544c89bd8c07bbc01b79f8304ea1165
Jessie S. Caminos <61927149+jsonimac@users.noreply.github.com>
2023-09-24 09:29:38 +0800

    update work image

--- src/components/homepage/works.jsx
+++ src/components/homepage/works.jsx
@@ -15,7 +15,7 @@ const Works = () => {
 					<div className="works-body">
 						<div className="work">
 							<img
-								src="https://scontent.fcgy2-2.fna.fbcdn.net/v/t39.30808-1/278498238_294100662908872_8539355154255964375_n.jpg?stp=dst-jpg_p320x320&_nc_cat=101&ccb=1-7&_nc_sid=754033&_nc_ohc=KkzorBzeZX4AX-Ms6HO&_nc_ht=scontent.fcgy2-2.fna&oh=00_AfARwY0JyrGrOry1FRkZR7ZCsbnzLlh4s4jWrYxRzeAuhw&oe=650B6D11"
+								src="human_inc.jpg"
 								alt="human incubator"
 								className="work-image"
 							/>
